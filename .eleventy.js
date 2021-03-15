7f68496ad72b243a383981eb454a320ff8896613
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2021-03-15 20:55:27 +0000

    add webp images

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./*.jpg");
+  eleventyConfig.addPassthroughCopy("./*.webp");
   eleventyConfig.addPassthroughCopy("./*.png");
   eleventyConfig.addPassthroughCopy("./*.ico");
 };
